537008d75a58b5abf055b6eceb1f30649a7a9ccf
patnamvamsi <patnamvamsi@gmail.com>
2021-08-14 16:05:28 +1000

    Initial commit

---
+++ reddit_data.py
@@ -0,0 +1,57 @@
+import praw
+import config
+
+equity_subreddits = 'wallstreetbets'
+crypto_subreddits = 'SatoshiStreetBets'
+
+reddit = praw.Reddit(client_id=config.REDDIT_CLIENT_ID,
+                     client_secret=config.REDDIT_SECRET,
+                     username=config.REDDIT_USERID,
+                     password=config.REDDIT_PASWWORD,
+                     user_agent=config.APP_NAME)
+
+equity_subreddit = reddit.subreddit(equity_subreddits)
+crypto_subreddit = reddit.subreddit(crypto_subreddits)
+
+hot_equity = equity_subreddit.hot(limit=10)
+hot_crypto = crypto_subreddit.hot(limit=10)
+
+'''
+Use recursion to retrive nested replies
+
+'''
+
+'''
+for submission in hot_crypto:
+
+    if not submission.stickied:
+        print (submission)
+        print(submission.title)
+        print(submission.comments._comments[0].body)
+        print (submission.comments._comments[0].reply)
+
+        print(submission.comments._comments[1].body)
+        print (submission.comments._comments[1].replies._comments[0].body)
+        print (submission.comments._comments[1].replies._comments[0].replies._comments[0])
+        print(submission.score) # upvotes-downvotes
+
+'''
+
+for comments in crypto_subreddit.stream.comments():
+    print("Submission: Title: " + comments.link_title)
+    print("Score: " + str(comments.score))
+    print("Comment: " + comments.body)
+
+'''Reddit Config
+https://www.youtube.com/watch?v=qCB8MZ-W1Ig&ab_channel=EatTheBlocks
+subs = ['wallstreetbets', 'SatoshiStreetBets']
+post_flairs = {'Daily Discussion','Weekend Discussion','Discussion'} # find more like news etc
+goodUser = {'AutoModerator'} # users who can comment more than once
+uniqueComment = True
+upvoteRatio = 0.7
+minCommentUpvotes = 2
+minReplyUpvotes = 20
+picks = 10 # define no of picks here , prints as "Top ## picks are:"
+picks_ayz = 5 #define number of picks for sentiment analysis
+'''
+
