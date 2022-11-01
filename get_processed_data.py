3a899cb6e4e57e577b3b0d9f2cb47f206ae8cf8e
Vamsi Patnam <patnamvamsi@gmail.com>
2022-11-01 23:40:28 +1100

    Refactor files Sentiment processing batchwise

--- get_processed_data.py
+++ get_processed_data.py
@@ -4,7 +4,7 @@ def get_processed_sentiment(twitter_handle):
     """
     Function to get the processed sentiment from the file.
     """
-    processed_sentiment = pd.read_csv('../data/output/' + twitter_handle + '.csv')
+    processed_sentiment = pd.read_csv('data/output/' + twitter_handle + '.csv')
     #processed_sentiment['timeline'] = processed_sentiment['timeline'].astype('Int64')
     return processed_sentiment
 
